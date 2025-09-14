4a4ccd29c5a2889d148b7b45022a7d6c40ab015d
Sunil Sharma <sunil3582@Sunils-MacBook-Air.local>
2025-09-14 15:12:49 +0530

    updated commit for the testing of app

---
+++ tests/test_app.py
@@ -0,0 +1,8 @@
+from app import add_numbers, is_even
+
+def test_add_numbers():
+    assert add_numbers(2, 3) == 5
+
+def test_is_even():
+    assert is_even(4) is True
+    assert is_even(5) is False
